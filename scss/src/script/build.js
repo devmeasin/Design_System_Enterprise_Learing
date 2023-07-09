c0faf3a92adc025300f63bcdad70dd8c6364aa92
Easin Webpro <easinislam299@gmail.com>
2023-07-09 21:12:43 +0600

    🎉 [DS.E] Step up Design System Project Initialize

---
+++ scss/src/script/build.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const path = require('path');
+const sass = require('sass');
+
+/**
+ * compile any scss file to css
+ * @param {string} src source file;
+ * @param {string} output output file
+ */
+
+
+const compile = (src , output) => {
+
+    const result = sass.compile(path.resolve(src),{
+		style: 'expanded',
+		verbose: true,
+	});
+
+    fs.writeFileSync(path.resolve(output), result.css);
+
+}
+
+// compile the global css
+compile('src/global.scss', 'lib/global.css');
+
+/**
+ * Get All Components from atoms, molecules and organisms
+ * @returns Object[] return array of object containing src and output
+ */
+
+const getComponents = () => {
+
+    let allComponents = [];
+
+    const types = ['atoms' , 'molecules', 'organisms'];
+
+    types.forEach((type) => {
+        const files = fs.readdirSync(path.resolve(`src/${type}`)).map((file) => ({
+            src : `src/${type}/${file}`,
+            output: `lib/${file.replace('.scss' , '.css')}`
+            // output: `lib/${type}/${file.replace('.scss' , '.css')}`
+        }));
+    
+        allComponents = [...allComponents , ...files];
+
+    });
+
+    return allComponents;
+
+} 
+
+
+// compile components
+getComponents().map((component) =>
+	compile(component.src, component.output)
+);
+
+// console.log(getComponents());
+
+
+
+// const files = fs.readdirSync(path.resolve(`src/${type}`));
+    
+//             files.forEach((file) => {
+    
+//                 if(file.endsWith('.scss')){
+    
+//                     allComponents.push({
+//                         name: file.replace('.scss' , ''),
+//                         type,
+//                     });
+    
+//                 }
+    
+//             })
